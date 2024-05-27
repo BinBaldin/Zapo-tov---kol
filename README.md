17bdac20e46df90283315e87bb00e956f7b36cd6
BinBaldin <tadeas.balda@gmail.com>
2024-05-27 10:59:31 +0200

    edit_of_readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Zápočotvý úkol pro předmět 18ZALG na ČVUT FJFI
+# Doubly Linked List
+Zápočotvý úkol pro předmět 18ZALG na ČVUT FJFI
+## Operace se seznamem
+* insert_at_begin - vložení na začátek
+* delete_at_begin - smazaní na začátku
+* 
--- main.py
+++ main.py
@@ -26,12 +26,10 @@ class DoublyLinkedList:
             self.head = new_node
         else:
             new_node = Node(data)
-            cur_node = self.head
-            while cur_node.next:
-                cur_node = cur_node.next
-            cur_node.next = new_node    
-            new_node.prev = cur_node
-            new_node.next = None                
+            self.head.prev = new_node
+            self.head.next = self.head
+            self.head = new_node    
+            new_node.prev = None          
 
         
     # smazání prvku na začátku, vrátí se hodnota v tomto prvku
